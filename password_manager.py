9923b3d6225a62d7fc583e5871dd8e1684361862
HamzaShakeel <mmhamza672@gmail.com>
2024-11-03 01:00:29 +0500

    Add password encryption setup and viewing functionality to password manager
    - Integrated cryptography module with Fernet encryption for password security.
    - Added write_key() function to generate and store an encryption key.
    - Implemented load_key() function to retrieve the encryption key for password encryption and decryption.
    - Completed iew() function to read and display stored usernames and passwords from password.txt.
    - Enhanced dd() function to save new account names and passwords to password.txt.

--- password_manager.py
+++ password_manager.py
@@ -1,11 +1,24 @@
-pwd = input ("What is master password?")
+master_pwd = input ("What is master password?")
 
 def view():
-    pass
+    with open("password.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip ()
+            user,passw =data.split("|")
+            print("User:",user,"| Password:",passw)
+    
+    
 def add():
-    pass
+    name = input("Account name: ")
+    pwd =input("Password: ")
+    
+    with open("password.txt","a") as f:
+        f.write(name+"|"+pwd+"\n")
+
+
+    
 while True:
-    mode = input("Would you like to add a new passwod or view existing ones(view/add),press q to quit").lower()
+    mode = input("Would you like to add a new passwod or view existing ones(view/add),press q to quit: ").lower()
     if mode=="q":
         break
     if mode=="view":
