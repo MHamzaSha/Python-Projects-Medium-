29d6d6ac013caba0aa9bea2579a0f928b2848713
HamzaShakeel <mmhamza672@gmail.com>
2024-11-02 19:18:17 +0500

    Add initial structure for password manager with view and add modes
    - Prompt user for a master password for security.
    - Set up main loop with options to view, add, or quit.
    - Defined placeholder functions for iew() and dd() to handle future functionality.
    - Implemented basic input validation for mode selection.

---
+++ password_manager.py
@@ -0,0 +1,9 @@
+pwd = input ("What is master password?")
+
+mode = input("Would you like to add a new passwod or view existing ones(view/add),press q to quit").lower()
+if mode=="q":
+    break
+if mode=="view":
+elif mode=="add":
+else:
+    print ("Invalid mode.")
